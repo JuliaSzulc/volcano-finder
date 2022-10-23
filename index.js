9f4610d62b5d43afa29d7d21a05f3e499a731aaa
Julia Szulc <me@juliaszulc.com>
2022-10-23 13:40:56 +0200

    Create nodejs express app

---
+++ index.js
@@ -0,0 +1,18 @@
+const { readFile } = require("fs");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use([express.static(__dirname), express.static(__dirname + '/data')]);
+
+app.get("/", (request, response) => {
+    readFile("index.html", "utf8", (err, html) => {
+        if (err) {
+            response.status(500).send("oh no")
+        }
+        response.send(html);
+    })
+})
+
+app.listen(port, () => console.log(`Available on port ${port}`))
